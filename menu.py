f646f9c6a9317a3a1ff0c7de75161a65e1aabdf9
JuanC20033002 <jacjunior11@gmail.com>
2026-03-30 17:50:45 -0600

    cc

--- menu.py
+++ menu.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
-def render_menu():
+def render_menu(pages_dict):
     with st.sidebar:
         st.markdown("## Economic Events Calendar")
         st.caption("Navigation")
-        st.page_link("pages/1_🚦_Traffic_Light.py", label="Traffic Light", icon="🚦")
-        st.page_link("pages/2_📈_Pandora_Universe.py", label="Pandora Universe", icon="📈")
-        st.page_link("pages/3_🗓️_Calendar.py", label="Calendar", icon="🗓️")
-        st.page_link("pages/4_🌐_External_News.py", label="External News", icon="🌐")
-        st.page_link("pages/5_📝_Assign_Dates.py", label="Assign Dates", icon="📝")
+
+        st.page_link(pages_dict["traffic_light"], label="Traffic Light", icon="🚦")
+        st.page_link(pages_dict["pandora_universe"], label="Pandora Universe", icon="📈")
+        st.page_link(pages_dict["calendar"], label="Calendar", icon="🗓️")
+        st.page_link(pages_dict["external_news"], label="External News", icon="🌐")
+        st.page_link(pages_dict["assign_dates"], label="Assign Dates", icon="📝")
