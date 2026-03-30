0c0076f5a2032de02f10df7e99a4c5d1cd3f0a19
JuanC20033002 <jacjunior11@gmail.com>
2026-03-30 17:24:10 -0600

    sss

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,11 +9,11 @@ st.set_page_config(
 
 navigation = st.navigation(
     [
-        st.Page("pages/1_🚦_Semaforo.py", title="Traffic Light", icon="🚦"),
+        st.Page("pages/1_🚦_Traffic_Light.py", title="Traffic Light", icon="🚦", default=True),
         st.Page("pages/2_📈_Pandora_Universe.py", title="Pandora Universe", icon="📈"),
-        st.Page("pages/3_🗓️_Calendario.py", title="Calendar", icon="🗓️"),
-        st.Page("pages/4_🌐_Noticias_Externas.py", title="External News", icon="🌐"),
-        st.Page("pages/5_📝_Asignar_Fechas.py", title="Assign Dates", icon="📝"),
+        st.Page("pages/3_🗓️_Calendar.py", title="Calendar", icon="🗓️"),
+        st.Page("pages/4_🌐_External_News.py", title="External News", icon="🌐"),
+        st.Page("pages/5_📝_Assign_Dates.py", title="Assign Dates", icon="📝"),
     ],
     position="sidebar",
 )
