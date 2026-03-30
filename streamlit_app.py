ac38906a385136075bf6f0cbe9c4ffb47aeb0f87
JuanC20033002 <jacjunior11@gmail.com>
2026-03-30 17:18:56 -0600

    sss

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,15 @@ st.set_page_config(
     initial_sidebar_state="expanded",
 )
 
-pg = st.navigation(
+navigation = st.navigation(
     [
         st.Page("pages/1_🚦_Semaforo.py", title="Traffic Light", icon="🚦"),
         st.Page("pages/2_📈_Pandora_Universe.py", title="Pandora Universe", icon="📈"),
         st.Page("pages/3_🗓️_Calendario.py", title="Calendar", icon="🗓️"),
         st.Page("pages/4_🌐_Noticias_Externas.py", title="External News", icon="🌐"),
         st.Page("pages/5_📝_Asignar_Fechas.py", title="Assign Dates", icon="📝"),
-    ]
+    ],
+    position="sidebar",
 )
 
-pg.run()
+navigation.run()
